6b40941b8c1925a41c55c82d82068ec9fe6bd6e3
P4ST4S <antoine.rospars@epitech.eu>
2025-05-26 10:55:41 +0200

    Update docker-compose cmd to docker compose

--- init_ssl.sh
+++ init_ssl.sh
@@ -3,20 +3,20 @@
 # Script pour initialiser les certificats SSL pour triosigno.com
 
 # Arrêter les conteneurs en cours d'exécution
-docker-compose down
+docker compose down
 
 # Démarrer seulement Nginx pour l'initialisation
-docker-compose up -d nginx
+docker compose up -d nginx
 
 # Attendre que Nginx démarre
 echo "Attente de démarrage de Nginx..."
 sleep 5
 
 # Obtenir le certificat SSL
-docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/html --email admin@triosigno.com --agree-tos --no-eff-email -d triosigno.com -d www.triosigno.com
+docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/html --email admin@triosigno.com --agree-tos --no-eff-email -d triosigno.com -d www.triosigno.com
 
 # Redémarrer tous les services
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose up -d
 
 echo "Configuration terminée ! Vérifiez que le site est accessible sur https://triosigno.com"
